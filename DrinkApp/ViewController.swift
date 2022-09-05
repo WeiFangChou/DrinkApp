78ca1f58dd21d411774f73d7235427629ff7f22f
WeiFang Chou <chou.wei.fang@gmail.com>
2022-09-05 22:43:31 +0800

    Initial Commit

---
+++ DrinkApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrinkApp
+//
+//  Created by WeiFangChou on 2022/9/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
